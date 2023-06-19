8a89803407d0512c098d730532a93df7fa81784a
SoarYu <349815610@qq.com>
2023-06-19 16:58:07 +0000

    update test

---
+++ go/docker-gs-ping-roach/go.mod
@@ -0,0 +1,11 @@
+module docker-gs-ping-roach
+
+go 1.16
+
+require (
+	github.com/cenkalti/backoff/v4 v4.1.3
+	github.com/cockroachdb/cockroach-go/v2 v2.1.1
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/ory/dockertest/v3 v3.10.0
+	github.com/stretchr/testify v1.8.0
+)
---
+++ go/docker-gs-ping/main_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRespondsWithLove(t *testing.T) {
+
+	pool, err := dockertest.NewPool("")
+	require.NoError(t, err, "could not connect to Docker")
+
+	resource, err := pool.Run("docker-gs-ping", "latest", []string{})
+	require.NoError(t, err, "could not start container")
+
+	t.Cleanup(func() {
+		require.NoError(t, pool.Purge(resource), "failed to remove container")
+	})
+
+	var resp *http.Response
+
+	err = pool.Retry(func() error {
+		resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("8080/tcp"), "/"))
+		if err != nil {
+			t.Log("container not ready, waiting...")
+			return err
+		}
+		return nil
+	})
+	require.NoError(t, err, "HTTP error")
+	defer resp.Body.Close()
+
+	require.Equal(t, http.StatusOK, resp.StatusCode, "HTTP status code")
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err, "failed to read HTTP body")
+
+	// Finally, test the business requirement!
+	require.Contains(t, string(body), "<3", "does not respond with love?")
+}
+
+func TestIntMinBasic(t *testing.T) {
+	ans := IntMin(2, -2)
+	if ans != -2 {
+		t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+	}
+}
+
+func TestIntMinTableDriven(t *testing.T) {
+	var tests = []struct {
+		a, b int
+		want int
+	}{
+		{0, 1, 0},
+		{1, 0, 0},
+		{2, -2, -2},
+		{0, -1, -1},
+		{-1, 0, -1},
+	}
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
+		t.Run(testname, func(t *testing.T) {
+			ans := IntMin(tt.a, tt.b)
+			if ans != tt.want {
+				t.Errorf("got %d, want %d", ans, tt.want)
+			}
+		})
+	}
+}
